d166f070b3b496ba374997a7cbb4d34ca61872ac
ibondarenko <ext-i.bondarenko@netbiscuits.com>
2013-12-05 12:26:34 +0200

    Created basic structure

---
+++ src/main/java/com/imapi/watermark/controller/WatermarkController.java
@@ -0,0 +1,7 @@
+package com.imapi.watermark.controller;
+
+/**
+ * Created by ibon on 05.12.13.
+ */
+public class WatermarkController {
+}
---
+++ src/main/java/com/imapi/watermark/service/WatermarkService.java
@@ -0,0 +1,7 @@
+package com.imapi.watermark.service;
+
+/**
+ * Created by ibon on 05.12.13.
+ */
+public class WatermarkService {
+}
